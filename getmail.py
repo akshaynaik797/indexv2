3e37eff14b966cbcb3226f92bcecb14df871469b
akshaynaik797 <akshaynaik797@gmail.com>
2020-11-19 10:43:09 +0530

    0.0.3 tested gmail api for required features added requirements.txt

--- getmail.py
+++ getmail.py
@@ -34,11 +34,11 @@ def main():
     service = build('gmail', 'v1', credentials=creds)
     aid = "ANGjdJ_aENQY_Japg6ghrVxEC_z-SREpdT9HM9JXCBifUdte1xscpyizWaNEHhI32dD_LsN-45pO5sOmRKgYELj6z0mUV-jTv3Cmd8uLngQyh7_LBPNhsbUQv7pNCOj7FhA56rU4D0_Bp27WUa2rD3gUDUnwOhomlrhPQFajOae5WDSLG8IU-r9X9KYYmmzuCKQOZeX1BDqsl4fWBQh-N-OlgpMfMbYnxb8Ef9RK_wGS6oYmIprpA4hvNa7WxFcpz-H-euntnjSgHp5YnRH7ZN8GwKA_PxYtwU2w6dt1KeSI9eFk1HPt3xaEMDi4hvxg0h6s3MpmfCACprBtwrYMGoPO1WEAbLeIMUTzFjr_tuRggGrt6_PJE9LuG-gNxaJdn-MBaviRbFMa8a7yKT0I"
 
-    attachment = service.users().messages().attachments().get(userId='me', messageId='175bb31880d1ab37',
-                                                                id=aid).execute()
-    data = attachment['data']
-    with open('a', 'wb') as f:
-        f.write(base64.urlsafe_b64decode(data))
+    # attachment = service.users().messages().attachments().get(userId='me', messageId='175bb31880d1ab37',
+    #                                                             id=aid).execute()
+    # data = attachment['data']
+    # with open('a', 'wb') as f:
+    #     f.write(base64.urlsafe_b64decode(data))
 
 
     q = "before:1600962027 after:1600961487"
@@ -50,8 +50,7 @@ def main():
         print("Message snippets:")
         for message in messages:
             msg = service.users().messages().get(userId='me', id=message['id']).execute()
-            attachment = service().users().messages().attachments().get(userId='me', messageId='175bb31880d1ab37',
-                                                                              id=aid).execute()
+
             pass
 
             print(msg['snippet'])
